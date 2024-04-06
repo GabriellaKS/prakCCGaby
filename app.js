f49ce0f97e9cebedd041d4971df34b99a93a687b
GabriellaKS <149553936+GabriellaKS@users.noreply.github.com>
2024-04-07 00:40:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = "0.0.0.0";
 
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Gabriella K.S\n 51021002\n Sistem Informasi");
+  res.send("Nama : Gabriella K.S <br> NIM : 51021002 <br> Sistem Informasi");
 });
 
 app.listen(PORT, HOST);
